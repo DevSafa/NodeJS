e5c816abc5c4f5987f65e744980d6efeeaf8a050
sbarkadev <sbarka.dev@gmail.com>
2021-09-10 19:48:54 +0100

    Promise chaining example

--- Promises/script.js
+++ Promises/script.js
@@ -1,26 +1,10 @@
 'use strict';
 
-//this example shows 10, 20 and 60 every 3 seconds
-//-----------------------------------------------
-function generateNumber(num) {
-    return new Promise ((resolve, reject) => {
-        setTimeout(() => {
-            resolve(num);
-        }, 3 * 1000);
-    });
-}
+//you want to perform the following asynchronous opeartions in sequence
+/*
+    1- Get the user from the database
+    2- Get the services of the selected user
+    3- calculate the service cost from the user's services
 
-generateNumber(10)
-    .then(result => {
-        console.log(result);
-        return generateNumber(result * 2);
-    })
-    .then((result) => {
-        console.log(result);
-        return generateNumber(result * 3);
-    })
-    .then(result => console.log(result));
-//-----------------------------------------------
-//Promise chainining syntax
-
+*/
 
