6d57ad7f02d2020ec6c4f1b0ae2fb8bcfdf24288
sbarkadev <sbarka.dev@gmail.com>
2021-09-11 12:31:53 +0100

    Promise chaining using then() method example

--- Promises/script.js
+++ Promises/script.js
@@ -8,3 +8,44 @@
 
 */
 
+//function to get the user from database
+function getUser(userId){
+    return new Promise((resolve, reject) =>{
+        console.log('Get the user from the database.');
+        setTimeout(() => {
+            resolve({
+                userId: userId,
+                username: 'admin'
+            });
+        },1000);
+    })
+}
+
+//function to getServices of a user
+function getServices(user){
+    return new Promise((resolve, reject) => {
+        console.log(`Get the services of ${user.username} from the API.`);
+        setTimeout(() => {
+            resolve(['Email', 'VPN', 'CDN']);
+        }, 3 * 1000);
+    });
+}
+
+//calculate the service cost
+
+function getServiceCost(services)
+{
+    return new Promise((resolve, reject) => {
+        console.log(`Calculate the service cost of ${services}.`);
+        setTimeout(() => {
+            resolve(services.length * 100);
+        }, 2 * 1000)
+    });
+}
+
+// Get the user from the database.
+
+getUser(100)
+    .then(getServices)
+    .then(getServiceCost)
+    .then(console.log);
