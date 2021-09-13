6934aa775f335ff1bfae84d7f3a5ba33943b05ae
sbarkadev <sbarka.dev@gmail.com>
2021-09-13 19:21:00 +0100

    define and use a function inside a new file

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,15 @@
-//what's comes back from require is the add function.
-const add = require('./utils.js');
+/*
 
-const sum = add(4, -2);
+    challenge :Define and use a function in a new file
 
-console.log(sum);
+    1- creates a new file called notes.js
+    2- create getNotes function that return s "Your notes..."
+    3- export getNotes function
+    4- From app.js , load in and call the function printing message to console
 
+*/
+const getNotes = require('./notes.js');
 
+const msg = getNotes();
+
+console.log(msg);
---
+++ notes-app/notes.js
@@ -0,0 +1,7 @@
+// const notes = () => "your notes...";
+
+const getNotes = function (){
+    return "Your Notes...";
+}
+
+module.exports = getNotes;
