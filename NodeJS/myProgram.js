040b8c5bdce57256bf677d3107496289266efd17
sbarkadev <sbarka.dev@gmail.com>
2021-09-13 13:12:34 +0100

    first script - nodejs

--- NodeJS/myProgram.js
+++
@@ -1,9 +0,0 @@
-console.log('Hello wolrd');
-
-let noun1 = "Node";
-let noun2 = "javascript";
-let verb1 = "provides";
-let noun3 = "computers";
-
-
-console.log(`${noun1} ${verb1} the ability to run ${noun2} programs on our own ${noun3} instead of just in the browser’s console or embedded in HTML.`);
