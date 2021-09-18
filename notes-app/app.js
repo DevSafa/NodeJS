6acb44e5c12da58c51cdaed2b198a2ea1dfa549e
sbarkadev <sbarka.dev@gmail.com>
2021-09-18 19:30:51 +0100

    Argument Parsing with Yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,16 +1,54 @@
 const chalk  = require('chalk');
+const yargs = require('yargs'); // load in the yargs library
 const getNotes = require('./notes.js');
 
-const command = process.argv[2];
+// console.log(process.argv)
 
-//parsing command line argument is not unique to our app
-//it will best look for an npm package that will take care of that for us
-console.log(process.argv);
-if (command === 'add'){
-    console.log('Adding note!');
-} else if (command === 'remove'){
-    console.log('Removing note!');
-}
+//Customize yargs version ; by accessing yargs and calling the version method
+yargs.version('1.1.0');
+
+
+//create add command suing yargs , it takes an object
+yargs.command({
+    command: 'add',
+    describe: 'add a new note',
+    //handler is the code that runs when someone uses add command
+    handler:  function (){
+        console.log('Adding a new note!')
+    }
+})
+
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler : function(){
+        console.log('Removing the note');
+    }
+})
+
+//create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log('Reading a note');
+    }
+})
+
+//create list command
+yargs.command({
+    command: 'list',
+    describe: 'list your notes',
+    handler: function(){
+        console.log('Listing out  all notes');
+    }
+})
+//setting up yargs to work with commands we need it to work with
+//add , remove, read , list
+
+
+console.log(yargs.argv)
 
 
 
