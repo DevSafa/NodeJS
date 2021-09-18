549395009657cae093df3c95d18e570050e199ef
sbarkadev <sbarka.dev@gmail.com>
2021-09-18 17:53:04 +0100

    Getting input from Users:  notes-app

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,42 +1,16 @@
-//require the package name : require will return alll of the stuff that the validator package provides us .
-
-const validator = require('validator'); // to use it , see documentation
-
+const chalk  = require('chalk');
 const getNotes = require('./notes.js');
 
-const msg = getNotes();
+const command = process.argv[2];
 
-console.log(msg);
+//parsing command line argument is not unique to our app
+//it will best look for an npm package that will take care of that for us
+console.log(process.argv);
+if (command === 'add'){
+    console.log('Adding note!');
+} else if (command === 'remove'){
+    console.log('Removing note!');
+}
 
-console.log(validator.isEmail('invalidEmail'));
-console.log(validator.isEmail('sa_ba_1997@gmail.com'));
-console.log(validator.isEmail('sa_bad_1997@gmdail.com'));
-console.log(validator.isEmail('example.com'));
 
-console.log("------------------");
 
-console.log(validator.isURL("https://tkharbi9.com"));
-console.log(validator.isURL("http://tkharbi9.com"));
-console.log(validator.isURL("http://tkharbi9."));
-console.log("------------------------------------");
-//load chalk into app.js
-//for npm packages we load modules into our code by using package name.
-const chalk  = require('chalk');
-console.log(chalk.green("SUCCESS!"));
-console.log(chalk.yellow("WARNING!"));
-console.log(chalk.red("FAILED!"));
-
-console.log(chalk.blue.bgRed.bold('Hello!'));
-
-console.log(chalk.green.bgWhite.bold("SUCCESS!"));
-console.log(chalk.yellow.bgWhite.bold("WARNING!"));
-console.log(chalk.red.bgWhite.bold("FAILED!"));
-
-const greenMsg = chalk.green.bold("Green Hello"); // or chalk.bold.green
-console.log(greenMsg);
-
-const greenMsg1 = chalk.green.inverse.bold('Green Hello!');
-console.log(greenMsg1);
-
-// const colors = require('colors');
-// console.log(colors.green("hheelloo"));
