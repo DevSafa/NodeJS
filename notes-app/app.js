d341d6b86e4d34b8afd3de1274d09f9b4a38aa7c
sbarkadev <sbarka.dev@gmail.com>
2021-09-19 18:30:44 +0100

    Argument parsing with yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -7,25 +7,27 @@ const getNotes = require('./notes.js');
 yargs.version('1.1.0');
 
 
-//create add command suing yargs , it takes an object
+//create add command 
 yargs.command({
     command: 'add',
     describe: 'add a new note',
-    //builder is an object where we can define all the options that we want the given comamdn to suport
-    //we need option title and body to add a note , so we will seting up as properties in the builder object 
     builder : {
         title: {
             describe: 'Note title',
-            //set to false bydefault
-            //you have to provide title for that command work correctly
             demandOption : true,
-            //make sure that title property is always a string
+            type: 'string',
+        },
+        // add new property in the builder object
+        body: {
+            describe: 'Note body',
+            //default value of demandOption is false
+            demandOption: true,
             type: 'string',
         }
     },
     handler:  function (argv){
-        console.log('Adding a new note!',argv);
-        
+        console.log('Title: ' + argv.title);
+        console.log('body: ' + argv.body);
     }
 })
 
@@ -55,11 +57,12 @@ yargs.command({
         console.log('Listing out  all notes');
     }
 })
-//setting up yargs to work with commands we need it to work with
-//add , remove, read , list
 
+//yargs.parse() goes to the process of parsing arguments will all the configuration details
+//you've provided with your yargs command calls above
+yargs.parse();
 
-console.log(yargs.argv)
+console.log(yargs.argv);
 
 
 
