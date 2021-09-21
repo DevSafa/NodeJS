da469103748d29a673fe5089fc4e011e6e76107b
sbarkadev <sbarka.dev@gmail.com>
2021-09-21 17:04:18 +0100

    using Core Modules

---
+++ playground/index.js
@@ -0,0 +1,11 @@
+/*
+    requiring fs module
+    to read and write data to filesystem.
+    fs is a builtin module in node.
+*/
+
+const fs = require('fs')
+
+const hello = 'Hello wolrd';
+
+console.log(hello);
