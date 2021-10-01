25f42d101331ea8c2dd2c393c0c54eb2e0361055
sbarkadev <sbarka.dev@gmail.com>
2021-10-01 19:58:44 +0100

    asynchrounous programming , callbacks

---
+++ weather-app/app.js
@@ -0,0 +1,11 @@
+console.log('Starting');
+
+//basic asynchrouns function that node provide
+//setTime out allows us to run code after  a specific amount of time has past
+
+// take  acallback , and a number of milliseconds you gonna wait until the callback 
+setTimeout(() => {
+
+}, )
+
+console.log('Stopping');
