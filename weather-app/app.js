39ad1ff047b8d34f94a1614242cb958e0744e428
sbarkadev <sbarka.dev@gmail.com>
2021-10-03 15:45:39 +0100

    Asynchrounous Basics

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,16 @@
 console.log('Starting');
 
-//basic asynchrouns function that node provide
-//setTime out allows us to run code after  a specific amount of time has past
+// use most basic asynchrouns function that node provide :
+//setTimeout allows us to run code after  a specific amount of time has past
 
-// take  acallback , and a number of milliseconds you gonna wait until the callback 
+// take  a callback , and a number of milliseconds you gonna wait until the callback will be executed
 setTimeout(() => {
+    console.log('2 Second Timer');
+}, 2000) // run the callback after two seconds has past
 
-}, )
-
+setTimeout(() =>{ //why 0 Second Timer comes after Stopping
+    console.log('0 Second Timer');
+}, 0);
+// if we are fetching data for one user it will be nice to keep doing other things in our program instead of 
+// waiting
 console.log('Stopping');
