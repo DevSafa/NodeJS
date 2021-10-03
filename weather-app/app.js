244e4d485a75e9b9a10aa44f62e24c8950fa576b
sbarkadev <sbarka.dev@gmail.com>
2021-10-03 18:58:26 +0100

    Customizing HTTP request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,26 +1,19 @@
-//explore the weather api
-//make our first request for real time weather data
-//make http request to a weather api in our node app
-
-//to make http request : use core node modules (very low level) , use npm modules that wrapper arround the core module
-
-// npm module to make http request
 const request = require('request');
 
 const url = 'http://api.weatherstack.com/current?access_key=93a509590a634d41eec32553a0695764&query=32.235967,-7.946685'
-
-//make request to get the data as variable in my node app 
-//use request function
-
-//first : option object => what we like to do provide url 
-//second : function to run when we have response
-
-request({ url : url}, (error, response) =>{
-   // console.log(response); // let's focus on the body property that represent the json string
-   // console.log(response.body);
-   //parse a JSON string
-    const data = JSON.parse(response.body);
-    console.log(data);
-    console.log("---------\n");
-    console.log(data.current);
+//automatically parse json responses for us 
+//json :true means that response body will always be an object , no need to parse it
+request({ url : url, json: true}, (error, response) =>{
+    console.log(response.body.current);
+    const forecast = `It is currently ${response.body.current.temperature} degrees out . There is a ${response.body.current.precip}%  `;
+    console.log(forecast);
 });
+
+//what type of forecast we want to print ?
+/*
+    Goal : Print a small forecast to the user
+
+    1- Print : "It is currently 58.55 degrees out. There is a 0% chance of rain."
+    2- Test your work
+
+*/
