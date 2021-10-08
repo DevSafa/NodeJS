1d3aaa0a88c29979f6fd89aeaf6ebf90cd08c977
sbarkadev <sbarka.dev@gmail.com>
2021-10-08 15:56:59 +0100

    Customizing HTTP Requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,13 @@
 const request = require('request');
 
-const url = 'http://api.weatherstack.com/current?access_key=93a509590a634d41eec32553a0695764&query=32.235967,-7.946685'
+const url = 'http://api.weatherstack.com/current?access_key=93a509590a634d41eec32553a0695764&query=32.235967,-7.946685&units=f'
 //automatically parse json responses for us 
 //json :true means that response body will always be an object , no need to parse it
+// request module automaticaaly parse body for us
+// url and json are objects
 request({ url : url, json: true}, (error, response) =>{
     console.log(response.body.current);
-    const forecast = `It is currently ${response.body.current.temperature} degrees out . There is a ${response.body.current.precip}%  `;
+    const forecast = response.body.current.weather_descriptions[0] + `. It is currently ${response.body.current.temperature} degrees out . it feels like ${response.body.current.feelslike} degrees out .`;
     console.log(forecast);
 });
 
@@ -13,7 +15,7 @@ request({ url : url, json: true}, (error, response) =>{
 /*
     Goal : Print a small forecast to the user
 
-    1- Print : "It is currently 58.55 degrees out. There is a 0% chance of rain."
+    1- Print : "It is currently 58.55 degrees out. It feels like 5 degrees out"
     2- Test your work
 
 */
