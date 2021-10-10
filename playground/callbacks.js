c9a0c231752b8de2829f419b5d7c250f4687ee1e
sbarkadev <sbarka.dev@gmail.com>
2021-10-10 13:22:15 +0100

    The callback Function

--- playground/callbacks.js
+++ playground/callbacks.js
@@ -13,7 +13,45 @@ setTimeout(() =>{
 
 //synchrounous way
 const names=['Safa','Sara','Jack'];
-const shortNames = names.filter(() => {
+const shortNames = names.filter((name) => {
     //return true if name has ength <=4
     return name.length <= 4; 
+});
+
+//--------------------------
+//define a function with callback
+/*const geocode =  (address, callback) => {
+
+    const data = {
+        latitude: 0,
+        longitude: 0,
+    }
+    return data;
+ 
+}
+const data = geocode("Philadelphia");
+console.log(data);
+*/
+
+
+//when the function is synchrounous we are able to use return 
+//when a function do asynchrounous stuff return it's no longer an option , instead of return it a value ,
+// we take a callback in , and call the callback with the value we ant when we have it
+const geocode =  (address, callback) => {
+    setTimeout(() => {
+        const data = {
+        latitude: 0,
+        longitude: 0,
+    }
+    //return will not work when we start do asynchrounous things inside our functions
+    // and here where the callback pattern will coming to play
+    //the callbacks functions , the ones that go to the node api ,a nd come down to the callback queue,
+    //they won't be executed until the call stack is empty , main has to finish
+    //return data;
+    callback(data);
+    },2000)
+}
+
+geocode("Philadelphia",(data) =>{
+    console.log(data);
 });
