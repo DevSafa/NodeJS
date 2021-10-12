d1adc86c7d497524bb38ae075d8ddfae03014d03
sbarkadev <sbarka.dev@gmail.com>
2021-10-12 14:06:09 +0100

    Callback chaining + get address by command line

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,13 +2,30 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 
-geocode('benguerir', (error,data) => {
+//Goal : Accept location via command line argument
+
+//1 - Access the command line argument without yargs
+//2 - Use the string value as the input of geocode
+//3 - only geocode if a location was provided
+//4 - test your work with a couple locations
+
+//first args : path to the node executable
+//second : the path to the string we wrote
+
+const address = process.argv[2];
+
+if (!address)
+    return console.log('Please Provide an address');
+
+geocode(address, (error,data) => {
     if(error)
         return console.log(error);
     forecast(data.latitude,data.longitude,(err,forecastdata) => {
         if(error)
             return console.log(error);
         console.log(data.location);
+       // console.log('latitude  : ',data.latitude);
+       // console.log('longitude : ',data.longitude);
         console.log(forecastdata);
     });
 });
