856887e64de479ae074941c5eac662553beda844
sbarkadev <sbarka.dev@gmail.com>
2021-10-12 12:00:37 +0100

    move the function to a file and export it to use it in other files

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,6 @@
 const request = require('request');
 
+const geocode = require('./utils/geocode');
 // const url = 'http://api.weatherstack.com/current?access_key=93a509590a634d41eec32553a0695764&query=32.235967,-7.946685&units=f'
 
 //wrong url :
@@ -70,33 +71,6 @@ const request = require('request');
 //     }
 // });
 
-//define a function and call it 
-//geocode will communicate with mapbox argument
-const geocode = (address, callback) => {
-   
-    //callback called once we have longitude and latitude
-    //encodeURIComponent is used for someone search for location that contains special caracteres that actually means someting in url structure  (? becomes %3F)
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) +'.json?access_token=pk.eyJ1Ijoic2JhcmthIiwiYSI6ImNrdWsweDUyNDMycGQyb21vZmZ0dWE3NTkifQ.mF0kLRehFcQJ1-5eR6_jjQ&limit=1';
-
-    //first argument of request is options object
-    //second argument is the function to run when the request is complete
-    request({url : url , json :true}, (error, response) => {
-        if (error) {
-            callback('Unable to connect to location services!',undefined); // undefined will be set by default by js 
-
-        }else if (response.body.features.length === 0)
-            callback('Unable to find location. Try another search.',undefined);
-        else
-        {
-            callback(undefined,{
-                latitude : response.body.features[0].center[0],
-                longitude : response.body.features[0].center[1],
-                location : response.body.features[0].place_name
-            })
-        }
-    })
-
-}
 
 geocode('12at', (error,data) => {
     console.log('Error : ', error);
@@ -114,6 +88,11 @@ geocode('Philadelphia New York', (error,data) => {
 });
 
 geocode('Philadelphia', (error,data) => {
+    console.log('Error : ', error);
+    console.log('Data  : ',data);
+});
+
+geocode('Boston', (error,data) => {
     console.log('Error : ', error);
     console.log('Data  : ',data);
 });
