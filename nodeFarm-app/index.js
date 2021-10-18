a303b3d26fc80ed2ae7a26fd3a7b4b4c8d3a48e0
sbarkadev <sbarka.dev@gmail.com>
2021-10-18 17:11:10 +0100

    read the file synchrounously + send back the json data to the user

--- nodeFarm-app/index.js
+++ nodeFarm-app/index.js
@@ -2,41 +2,33 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+//we will use the synchrounous version : it blocks the code execution => that is true.
+//but in this case , it's not a problem at all , that's because the top level code only gets executed once in the begening (once we start the program)
+// not like the callback function inside createServer that will be executed each time we create a request
+    // fs.readFile(`${__dirname}/data/data.json`,'utf-8',(err,data) => {
+    //     const productData = JSON.parse(data);
+    //     res.writeHead(200 ,{'content-type' : 'application/json'});
+    //     res.end(data);
+        
+    // });
+
+
+const data = fs.readFileSync(`${__dirname}/data/data.json`,'utf-8');
+const dataObj = JSON.parse(data); // parse it into an object 
+
 const server = http.createServer((req, res) => {
 
     console.log(req.url);
 
     const pathName = req.url;
-    //routing : for differen pathnames we have different actions
     if(pathName === '/' || pathName === '/overview')
         res.end('This is the OVERVIEW');
     else if (pathName === '/product')
         res.end('This is the PRODUCT');
     else if (pathName === '/api')
     {
-        //we want to read the data from the file data.json
-        //then parse json into javascript , and then send back that result to the client
-        
-        //read the file using fileread function
-        //the dot in node refers the directory from which we run the node command in the terminal
-        //if we start the script from the desktop , the dot will refer to Desktop
-            //fs.readFile('./data/data.json')
-        //all node js script get access a variable called __dirname
-        //this variable always translate to the directory in which the script is currently executed is located
-        fs.readFile(`${__dirname}/data/data.json`,'utf-8',(err,data) => {
-            //after finishing reading file the callabck is called
-            //data is in json => need to parse the data 
-            //JSON.parse() => take the json data which is a string and will automatically turened it into javascript object , or array in our case
-            const productData = JSON.parse(data);
-            console.log(productData);
-            //tell the browser that we're sending back a json
-            //200 stands for OK
-            res.writeHead(200 ,{'content-type' : 'application/json'});
-            //send back the data 
-            res.end(data);
-            
-            //each time someone hits /api route , the file will have to be read , so we need to read the file once
-        });
+       res.writeHead(200, {'content-type' : 'application/json'});
+       res.end(data);
     }
     else{
         res.writeHead(404 ,{
