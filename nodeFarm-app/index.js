53b452cba9f156288944dfc6c6be646f0b69104b
Safa Barka <sbarka@e1r12p12.1337.ma>
2021-10-21 16:43:37 +0100

    importing and exporting your own modules

--- nodeFarm-app/index.js
+++ nodeFarm-app/index.js
@@ -2,19 +2,9 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-const replaceTemplate =(template, product) => {
-    let output = template.replace(/{%PRODUCTNAME%}/g , product.productName);
-    output = output.replace(/{%IMAGE%}/g , product.image);
-    output = output.replace(/{%PRICE%}/g , product.price);
-    output = output.replace(/{%NUTRIENTS%}/g , product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g , product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g , product.description);
-    output = output.replace(/{%ID%}/g , product.id);
-    output = output.replace(/{%FROM%}/g , product.from);
-    if(!product.organic)
-        output = output.replace(/{%NOT_ORGANIC%}/g , 'not-organic');
-    return output;
-}
+//import my own module(after importing the core modules)
+// in require function the dot means the current location of this module
+const replaceTemplate = require('./modules/replaceTemplate');
 
 const data = fs.readFileSync(`${__dirname}/data/data.json`,'utf-8');
 const dataObj = JSON.parse(data);
---
+++ nodeFarm-app/modules/replaceTemplate.js
@@ -0,0 +1,17 @@
+//export this function , the replace function from this module 
+//module is an object that we have access to on each module (file)
+
+module.exports = (template, product) => {
+    let output = template.replace(/{%PRODUCTNAME%}/g , product.productName);
+    output = output.replace(/{%IMAGE%}/g , product.image);
+    output = output.replace(/{%PRICE%}/g , product.price);
+    output = output.replace(/{%NUTRIENTS%}/g , product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g , product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g , product.description);
+    output = output.replace(/{%ID%}/g , product.id);
+    output = output.replace(/{%FROM%}/g , product.from);
+    if(!product.organic)
+        output = output.replace(/{%NOT_ORGANIC%}/g , 'not-organic');
+    return output;
+}
+
